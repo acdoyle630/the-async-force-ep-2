bc3ebb8f7a6a5ac41daa8bff58647e2e7a6dbe99
Adam <acdoyle630@gmail.com>
2017-04-10 12:27:28 -1000

    on click determine resource type, and console.log approriate test

--- js/app.js
+++ js/app.js
@@ -1,2 +1,22 @@
 /*jshint esversion: 6*/
 
+var requestInfo = document.querySelector('#requestResourceButton');
+let contentDisplay;
+
+requestInfo.addEventListener('click', function(){
+  let display = document.querySelector('#contentContainer');
+  if(resourceType.value === 'people'){
+    people();
+  }
+  if(resourceType.value === 'planets'){
+    planets(num);
+  }
+  if(resourceType.value === 'starships'){
+    starships(num);
+  }
+  display.innerHTML = contentDisplay;
+});
+
+function people(){
+  console.log('people test');
+}
